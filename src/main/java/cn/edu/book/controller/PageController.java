e65d93284beb53c411f625ec5bea71125fba3259
sukun120 <su209@qq.com>
2019-12-30 20:41:42 +0800

    1.完善文件夹 2.更改application.properties

--- src/main/java/cn/edu/book/controller/PageController.java
+++ src/main/java/cn/edu/book/controller/PageController.java
@@ -1,7 +1,10 @@
 package cn.edu.book.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Wu on 2019/12/30.
@@ -12,8 +15,12 @@ public class PageController {
     public String index(){
         return "index";
     }
-    @RequestMapping("/login")
-    public String login(){
+    @GetMapping("/login")
+    public String login(Model model, @RequestParam(value = "error", required = false) String error){
+        if (error != null) {
+            model.addAttribute("error", "用户名或密码错误");
+        }
         return "login";
     }
+
 }
