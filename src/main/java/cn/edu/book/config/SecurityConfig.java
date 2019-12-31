3b9a755a67e0ed236124eca1760d5e588124f803
sukun120 <su209@qq.com>
2019-12-31 12:33:03 +0800

    1.写了学院的实体类,mapper,和Mapper.xml，实现学院的增删改查

--- src/main/java/cn/edu/book/config/SecurityConfig.java
+++ src/main/java/cn/edu/book/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package cn.edu.book.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -14,20 +16,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //首页所有人可以访问，对应功能页只有对应有权限的人可以访问
         //请求授权规则
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll();
-//        .antMatchers("/login").hasRole("manager")
-
         //没有权限默认会到登陆页面
         // /login
-        http.formLogin().loginPage("/login");
-        //防网站攻击关闭
-        //logout失败可能导致原因
-        http.csrf().disable();
-        // 开启注销功能
-        http.logout().logoutSuccessUrl("/");
-
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .and().formLogin().loginPage("/login")
+                .permitAll()                   //跳转登录页面的控制器，该地址要保证和表单提交的地址一致！
+                .and()
+                .logout().permitAll()         // 开启注销功能
+                 .and()
+                .csrf().disable();            //防网站攻击关闭,logout失败可能导致原
 
     }
 
@@ -41,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        可从缓存中添加认证
 //        也可从JDBC添加认证
         auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("manager").password(new BCryptPasswordEncoder().encode("123456")).roles("manager")
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("123456")).roles("admin")
                 .and()
                 .withUser("teacher").password(new BCryptPasswordEncoder().encode("123456")).roles("teacher")
                 .and()
