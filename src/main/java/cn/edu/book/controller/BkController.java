3138aeabe6162e0c760bbeac1715bf619b644645
Wux <574603760@qq.com>
2020-01-06 14:25:30 +0800

    修改页面

--- src/main/java/cn/edu/book/controller/BkController.java
+++ src/main/java/cn/edu/book/controller/BkController.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/bkadmin")
@@ -94,7 +97,43 @@ public class BkController {
         String str = JSON.toJSONString(s);
         return str;
     }
-
+    @RequestMapping("/listkucun")
+    @ResponseBody
+    public String listkucun(){
+        List<Bk> list = bkMapper.getAllBk();
+        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> mapNow = new HashMap<>();
+        List<String> bookName = new ArrayList<>();
+        List<Integer> allNum = new ArrayList<>();
+        List<Integer> nowNum = new ArrayList<>();
+        for (int i = 0;i<list.size();i++){
+            if (!map.containsKey(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor())){
+                map.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(), list.get(i).getBkNum());
+            }else{
+                int value = map.get(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor());
+                map.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(),value+list.get(i).getBkNum());
+            }
+            if (!mapNow.containsKey(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor())){
+                mapNow.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(), list.get(i).getBkNumNow());
+            }else{
+                int value = mapNow.get(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor());
+                mapNow.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(),value+list.get(i).getBkNumNow());
+            }
+        }
+        for (Map.Entry<String, Integer> map1:map.entrySet()){
+//            System.out.println(map1.getValue());
+//            System.out.println(map1.getKey());
+        }
+        for (Map.Entry<String, Integer> map1:mapNow.entrySet()){
+            bookName.add(map1.getKey());
+            allNum.add(map.get(map1.getKey()));
+            nowNum.add(map1.getValue());
+        }
+        String bkName  = JSON.toJSONString(bookName);
+        String allNumBk  = JSON.toJSONString(allNum);
+        String nowNumBk  = JSON.toJSONString(nowNum);
+        return bkName+"|"+allNumBk+"|"+nowNumBk;
+    }
 
 
 }
--- src/test/java/cn/edu/book/mapper/UserMapperTest.java
+++ src/test/java/cn/edu/book/mapper/UserMapperTest.java
@@ -1,13 +1,18 @@
 package cn.edu.book.mapper;
 
+import cn.edu.book.model.Bk;
 import cn.edu.book.model.User;
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -18,10 +23,48 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class UserMapperTest {
    @Autowired
     private UserMapper userMapper;
+   @Autowired
+   private BkMapper bkMapper;
+
    @Test
     public void ss(){
        User user = userMapper.findByName("admin");
        System.out.println(user.toString());
    }
-
+    @Test
+    public void zzz(){
+        List<Bk> list = bkMapper.getAllBk();
+        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> mapNow = new HashMap<>();
+        List<String> bookName = new ArrayList<>();
+        List<Integer> allNum = new ArrayList<>();
+        List<Integer> nowNum = new ArrayList<>();
+        for (int i = 0;i<list.size();i++){
+            if (!map.containsKey(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor())){
+                map.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(), list.get(i).getBkNum());
+            }else{
+                int value = map.get(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor());
+                map.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(),value+list.get(i).getBkNum());
+            }
+            if (!mapNow.containsKey(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor())){
+                mapNow.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(), list.get(i).getBkNumNow());
+            }else{
+                int value = mapNow.get(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor());
+                mapNow.put(list.get(i).getBkName()+"-"+list.get(i).getBkAuthor(),value+list.get(i).getBkNumNow());
+            }
+        }
+        for (Map.Entry<String, Integer> map1:map.entrySet()){
+//            System.out.println(map1.getValue());
+//            System.out.println(map1.getKey());
+        }
+        for (Map.Entry<String, Integer> map1:mapNow.entrySet()){
+            bookName.add(map1.getKey());
+            allNum.add(map.get(map1.getKey()));
+            nowNum.add(map1.getValue());
+        }
+        String bkName  = JSON.toJSONString(bookName);
+        String allNumBk  = JSON.toJSONString(allNum);
+        String nowNumBk  = JSON.toJSONString(nowNum);
+        //return bkName+"|"+allNumBk+"|"+nowNumBk;
+    }
 }
